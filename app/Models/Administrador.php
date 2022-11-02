2c687e372756e8212cebdb995a43d9397120c4ed
matro1234 <74157597+matro1234@users.noreply.github.com>
2022-11-02 19:54:22 +0100

    Update Administrador.php

--- app/Models/Administrador.php
+++ app/Models/Administrador.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Administrador extends Model
 {
     use HasFactory;
+    protected $table="administrador";
+    protected $primaryKey="CODADMINISTRADOR";
+    protected $fillable = ["CONTRASENA","NOMBREADMINISTRADOR","API_TOKEN"];
+    public $timestamps = false;
 }
